0b70a7e01e44fa181f1c92edb272004e8112211f
bag0bits <bag0bits@iwantmym.tv>
2021-09-03 23:12:28 +0000

    download conf for 1.29.0

--- conf/download_config_files.sh
+++ conf/download_config_files.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-wget https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-config.json
-wget https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-byron-genesis.json
-wget https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-shelley-genesis.json
-wget https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/mainnet-topology.json
+wget https://hydra.iohk.io/build/7416228/download/1/mainnet-config.json
+wget https://hydra.iohk.io/build/7416228/download/1/mainnet-byron-genesis.json
+wget https://hydra.iohk.io/build/7416228/download/1/mainnet-shelley-genesis.json
+wget https://hydra.iohk.io/build/7416228/download/1/mainnet-alonzo-genesis.json
+wget https://hydra.iohk.io/build/7416228/download/1/mainnet-topology.json
 sed -i mainnet-config.json -e "s/TraceBlockFetchDecisions\": false/TraceBlockFetchDecisions\": true/g"
+
