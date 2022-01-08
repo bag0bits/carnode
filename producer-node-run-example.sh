77abdfbcc245ccb13b10104b613b1da20d162cba
bag0rice <bag0rice@iwantmym.tv>
2022-01-08 07:31:56 -0500

    updateing startup scripts

--- producer-node-run-example.sh
+++ producer-node-run-example.sh
@@ -9,15 +9,27 @@ VRF=${NODE_HOME}/vrf.skey
 CERT=${NODE_HOME}/node.cert
 HOSTADDR=0.0.0.0
 PORT=3001
+EKG_PORT=4001
+PROMETHEUS_PORT=5001
+CARVER=1.33.0
+THREADS=8
 
-docker run -ti --rm --name producer --entrypoint ${CNODE} -p ${PORT}:3001 -v $(pwd):${NODE_HOME} carnode run \
-	 --topology ${TOPOLOGY} \
-	 --database-path ${DB_PATH} \
-	 --socket-path ${SOCKET_PATH} \
-	 --host-addr ${HOSTADDR} \
-	 --port 3001 \
-	 --config ${CONFIG} \
-	 +RTS -N -RTS \
-         --shelley-kes-key ${KES} \
-         --shelley-vrf-key ${VRF} \
-         --shelley-operational-certificate ${CERT} 
+docker run -tid --rm --name producer \
+	--log-opt max-size=512m \
+	--entrypoint ${CNODE} \
+	-p ${PORT}:3001 \
+	-p ${PROMETHEUS_PORT}:12798 \
+	-v $(pwd):${NODE_HOME} \
+	carnode:${CARVER} run \
+		--topology ${TOPOLOGY} \
+		--database-path ${DB_PATH} \
+		--socket-path ${SOCKET_PATH} \
+		--host-addr ${HOSTADDR} \
+		--port ${PORT} \
+		--config ${CONFIG} \
+		--shelley-kes-key ${KES} \
+		--shelley-vrf-key ${VRF} \
+		+RTS -N${THREADS} -RTS \
+		--shelley-operational-certificate ${CERT} 
+
+docker logs -f producer > producer.log &
--- relay-node-run-example.sh
+++ relay-node-run-example.sh
@@ -6,12 +6,24 @@ DB_PATH=${NODE_HOME}/db
 SOCKET_PATH=${NODE_HOME}/socket
 HOSTADDR=0.0.0.0
 PORT=3002
+EKG_PORT=4002
+PROMETHEUS_PORT=5002
+CARVER=1.33.0
+THREADS=8
 
-docker run -tid --rm --name relay1 --entrypoint ${CNODE} -p ${PORT}:3001 -v $(pwd):${NODE_HOME} carnode run \
-	 --topology ${TOPOLOGY} \
-	 --database-path ${DB_PATH} \
-	 --socket-path ${SOCKET_PATH} \
-	 --host-addr ${HOSTADDR} \
-	 --port 3001 \
-	 --config ${CONFIG} \
-	 +RTS -N -RTS
+docker run -tid --rm --name producer \
+	--log-opt max-size=512m \
+	--entrypoint ${CNODE} \
+	-p ${PORT}:3001 \
+	-p ${PROMETHEUS_PORT}:12798 \
+	-v $(pwd):${NODE_HOME} \
+	carnode:${CARVER} run \
+		--topology ${TOPOLOGY} \
+		--database-path ${DB_PATH} \
+		--socket-path ${SOCKET_PATH} \
+		--host-addr ${HOSTADDR} \
+		--port ${PORT} \
+		--config ${CONFIG} \
+		+RTS -N${THREADS} -RTS
+
+docker logs -f relay1 > relay1.log &
