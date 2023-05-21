8e760c601c379d2de6a7fed55e04413cf7f9676a
bag0bits <bag0bits@iwantmym.tv>
2023-05-21 20:36:41 +0000

    add script to get 8.0.0 config files

---
+++ get_config.sh
@@ -0,0 +1,8 @@
+wget https://book.world.dev.cardano.org/environments/shelley-qa/config.json
+wget https://book.world.dev.cardano.org/environments/shelley-qa/db-sync-config.json
+wget https://book.world.dev.cardano.org/environments/shelley-qa/submit-api-config.json
+wget https://book.world.dev.cardano.org/environments/shelley-qa/topology.json
+wget https://book.world.dev.cardano.org/environments/shelley-qa/byron-genesis.json
+wget https://book.world.dev.cardano.org/environments/shelley-qa/shelley-genesis.json
+wget https://book.world.dev.cardano.org/environments/shelley-qa/alonzo-genesis.json
+wget https://book.world.dev.cardano.org/environments/shelley-qa/conway-genesis.json
